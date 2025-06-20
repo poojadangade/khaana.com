0f3f985fef13c232f4fffb72cdb92d4991d72b14
Pooja Dangade <pddangade@gmail.com>
2025-06-21 01:23:13 +0530

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Button from '@mui/material/Button';
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <Button variant="contained">Hello world</Button>;
         <Image
           className={styles.logo}
           src="/next.svg"
